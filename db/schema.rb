400dc38ac3fe94490f59aebe705197df8c5948c0
Raggnell <hudjamalkhan@gmail.com>
2015-10-10 18:41:04 +0000

    Added Support For Listing User Link

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151005204812) do
+ActiveRecord::Schema.define(version: 20151010182418) do
 
   create_table "listings", force: :cascade do |t|
     t.string   "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20151005204812) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "image"
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
