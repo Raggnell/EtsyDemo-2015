550775aa781b86c2bad43e79ddbafe793594cfe4
Raggnell <hudjamalkhan@gmail.com>
2015-10-26 21:25:34 +0000

    Enabled Users To Place Orders

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151010182418) do
+ActiveRecord::Schema.define(version: 20151026204013) do
 
   create_table "listings", force: :cascade do |t|
     t.string   "name"
@@ -21,6 +21,17 @@ ActiveRecord::Schema.define(version: 20151010182418) do
     t.datetime "updated_at",  null: false
     t.string   "image"
     t.integer  "user_id"
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.string   "address"
+    t.string   "city"
+    t.string   "state"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "listing_id"
+    t.integer  "buyer_id"
+    t.integer  "seller_id"
   end
 
   create_table "users", force: :cascade do |t|
