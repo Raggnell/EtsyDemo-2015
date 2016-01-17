cb21a106e305c636b301ba45df8ef19596239a3b
Raggnell <hudjamalkhan@gmail.com>
2016-01-17 21:04:42 +0000

    Enable users to place orders

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   resources :orders
+  resources :orders
 
   devise_for :users
   resources :listings do
@@ -10,6 +11,8 @@ Rails.application.routes.draw do
   get 'pages/about'
   get 'pages/contact'
   get 'seller' => "listings#seller"
+  get 'sales' => "orders#sales"
+  get 'purchases' => "orders#purchases"
 
   root 'listings#index'
 
